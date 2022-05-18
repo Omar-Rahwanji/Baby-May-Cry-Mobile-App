ed883e6a9b4b83e52aa1e0a3963080a2f1fdca5f
Omar Rahwanji <rahwanjiomar@gmail.com>
2022-05-18 04:11:38 +0300

    Finish api

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -6,8 +6,8 @@ import 'package:http/http.dart' as http;
 import 'package:localize_and_translate/localize_and_translate.dart';
 
 Future<Map<String,dynamic>> getCryReason() async {
-  // http.Response cryReason = await http.get(Uri.parse('https://babymaycry.herokuapp.com'));
-  http.Response cryReason = await http.get(Uri.parse('http://10.0.2.2:5000'));
+  http.Response cryReason = await http.get(Uri.parse('https://babymaycry.herokuapp.com'));
+  // http.Response cryReason = await http.get(Uri.parse('http://10.0.2.2:5000'));
 
   return json.decode(cryReason.body) as Map<String,dynamic>;
 }
@@ -16,8 +16,8 @@ Future<bool> sendCrySound(String crySoundPath) async {
   // var headers = {'Content-Type': 'audio/wav'};
   var request = http.MultipartRequest(
     'POST',
-    // Uri.parse('https://babymaycry.herokuapp.com'),
-    Uri.parse('http://10.0.2.2:5000'),
+    Uri.parse('https://babymaycry.herokuapp.com'),
+    // Uri.parse('http://10.0.2.2:5000'),
 
   );
 
